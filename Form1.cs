63c9df3a60562e8a97d198aab9356c3f1ef823e2
sarahduv <sarah.duvdevani@gmail.com>
2020-02-07 18:30:28 -0800

    cleaning up

--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,7 @@ namespace XInARow
     public partial class Form1 : Form
     {
         public String currentPlayer = "X";
-        public List<PictureBox> allTiles;
+        public PictureBox[] allTiles;
         public PictureBox[][] possibleWins;
         public String[,] currentStateOfBoard;
         public String winningPlayer;
@@ -26,13 +26,14 @@ namespace XInARow
         public Form1()
         {
             InitializeComponent();
-            allTiles = new List<PictureBox>();
-            
-            foreach(var pb in tableLayoutPanel1.Controls.OfType<PictureBox>())
+            allTiles = new PictureBox[gridSize * gridSize];
+
+            foreach (var pb in tableLayoutPanel1.Controls.OfType<PictureBox>())
             {
                 if (pb.Name.StartsWith("pictureBox"))
                 {
-                    allTiles.Add(pb);
+                    var pbIndex = int.Parse(pb.Name.Replace("pictureBox", "")) - 1;
+                    allTiles[pbIndex] = pb;
                 }
             }
 
@@ -42,7 +43,6 @@ namespace XInARow
 
         private void playerChoosesTile(object sender, EventArgs e)
         {
-            Debug.WriteLine("current:" + currentStateOfBoard[1, 1]);
             if (hasWon || draw)
             {
                 MessageBox.Show("Reset the game");
@@ -50,53 +50,37 @@ namespace XInARow
             }
 
             var tileToPlay = (PictureBox)sender;
-
-            if (currentPlayer == "X" && tileToPlay.Tag == null)
-            {
-                tileToPlay.Image = Properties.Resources.x;
-                tileToPlay.Tag = "X";
-
-                if (checkForWin(tileToPlay))
-                {
-                    MessageBox.Show("Win: " + tileToPlay.Tag);
-                }
-
-                if (thereIsADraw())
-                {
-                    MessageBox.Show("It's a draw");
-                    currentTurnLabel.Text = "Draw!";
-                    currentTurnImage.Image = null;
-                    return;
-                }
-
-                currentPlayer = "O";
-                currentTurnImage.Image = Properties.Resources.o;
-            }
-            else if (currentPlayer == "O" && tileToPlay.Tag == null)
-            {
-                tileToPlay.Image = Properties.Resources.o;
-                tileToPlay.Tag = "O";
-
-                if (checkForWin(tileToPlay))
-                {
-                    MessageBox.Show("Win: " + tileToPlay.Tag);
-                }
-
-                if (thereIsADraw())
-                {
-                    MessageBox.Show("It's a draw");
-                    currentTurnLabel.Text = "Draw!";
-                    currentTurnImage.Image = null;
-                    return;
-                }
-
-                currentPlayer = "X";
-                currentTurnImage.Image = Properties.Resources.x;
-            }
-            else
+            if (tileToPlay.Tag != null)
             {
                 MessageBox.Show("This tile is taken, choose another tile.");
+                return;
             }
+
+            var nextPlayer = currentPlayer == "X" ? "O" : "X";
+            var nextPlayerImage = currentPlayer == "X" ? Properties.Resources.o : Properties.Resources.x;
+            var currentPlayerImage = currentPlayer == "X" ? Properties.Resources.x : Properties.Resources.o;
+            
+            // Put the current player's image in the tile
+            tileToPlay.Image = currentPlayerImage;
+            tileToPlay.Tag = currentPlayer;
+
+            // Check for win or draw
+            if (checkForWin(tileToPlay))
+            {
+                MessageBox.Show("Win: " + tileToPlay.Tag);
+                return;
+            } 
+            else if (thereIsADraw())
+            {
+                MessageBox.Show("It's a draw");
+                currentTurnLabel.Text = "Draw!";
+                currentTurnImage.Image = null;
+                return;
+            }
+
+            // Round finished, update to show it's next player's turn
+            currentPlayer = nextPlayer;
+            currentTurnImage.Image = nextPlayerImage;
         }
 
         private bool checkForWin(PictureBox tileLastPlayed)
@@ -105,16 +89,12 @@ namespace XInARow
             var tileRow = getTileRow(tileLastPlayed);
             var tileCol = getTileCol(tileLastPlayed);
 
-            bool rowWin = getResult(getRowArray(tileRow, tileLastPlayed));
-            bool colWin = getResult(getColArray(tileCol, tileLastPlayed));
-            bool diagWin = getResult(getDiagArray(tileRow, tileCol, tileLastPlayed));
-
-            if(rowWin || colWin || diagWin)
-            {
-                return true;
-            }
-
-            return false;
+            return (
+                getResult(getRowArray(tileRow, tileLastPlayed)) ||
+                getResult(getColArray(tileCol, tileLastPlayed)) ||
+                getResult(getDiagArray(tileRow, tileCol, tileLastPlayed, true)) ||
+                getResult(getDiagArray(tileRow, tileCol, tileLastPlayed, false))
+            );
         }
 
         private bool[] getRowArray(int row, PictureBox tile)
@@ -123,11 +103,7 @@ namespace XInARow
 
             for(var i = 0; i < gridSize; i++) // gridSize = 10
             {
-                if (currentStateOfBoard[row, i] != (String)tile.Tag)
-                {
-                    rowArr[i] = false;
-                }
-                else { rowArr[i] = true; };
+                rowArr[i] = currentStateOfBoard[row, i] == (String)tile.Tag;
             }
 
             return rowArr;
@@ -139,124 +115,107 @@ namespace XInARow
 
             for (var i = 0; i < gridSize; i++) // gridSize = 10
             {
-                if (currentStateOfBoard[i, col] != (String)tile.Tag)
-                {
-                    colArr[i] = false;
-                }
-                else { colArr[i] = true; };
+                colArr[i] = currentStateOfBoard[i, col] == (String)tile.Tag;
             }
 
             return colArr;
         }
 
-        private bool[] getDiagArray(int row, int col, PictureBox tile)
+      /*  private bool[] getDiagArray2(int row, int col, PictureBox tile, bool isDownRight)
+        {
+            bool[] diag = new bool[10];
+
+            var colDirection = isDownRight ? 1 : -1;
+
+            diag[col] = true;
+            for (int distance = 1; distance < gridSize; distance++)
+            {
+                var rowUp = row - distance;
+                var rowDown = row + distance;
+                var testedCol = col + (distance * colDirection);
+
+            }
+
+            return diag;
+        }
+*/
+        private bool[] getDiagArray(int row, int col, PictureBox tile, bool isDownRight)
         {
             List<bool> diagListLower = new List<bool>();
-            int lowerRow = row + 1; // adjust to be offset lower than the current tile
-            int lowerCol = col + 1;
-
-            while(lowerCol < gridSize && lowerRow < gridSize)
-            {
-                if (currentStateOfBoard[lowerRow, lowerCol] != (String)tile.Tag)
-                {
-                    diagListLower.Add(false);
-                }
-                else { diagListLower.Add(true); };
-            }
-
-            diagListLower.Reverse();
-
             List<bool> diagListUpper = new List<bool>();
-            int upperRow = row - 1;
-            int upperCol = col - 1;
 
-            while(upperCol > 0 && upperRow > 0)
+            var colDirection = isDownRight ? 1 : -1;
+
+            int lowerRow = row + 1; // adjust to be offset 1 col right and 1 row below
+            int lowerCol = col + colDirection;
+
+            int upperRow = row - 1; // adjust to be offset 1 col left and 1 row higher
+            int upperCol = col - colDirection;
+
+
+            while (lowerCol >= 0 && lowerRow >= 0 && lowerCol < gridSize && lowerRow < gridSize)
             {
-                if (currentStateOfBoard[upperRow, upperCol] != (String)tile.Tag)
-                {
-                    diagListUpper.Add(false);
-                }
-                else { diagListUpper.Add(true); };
+                diagListLower.Add(currentStateOfBoard[lowerRow, lowerCol] == (String)tile.Tag);
+                lowerRow += 1;
+                lowerCol += colDirection;
             }
+
+
+            while(upperCol >= 0 && upperRow >= 0 && upperCol < gridSize && upperRow < gridSize)
+            {
+                diagListUpper.Add(currentStateOfBoard[upperRow, upperCol] == (String)tile.Tag);
+                upperRow -= 1;
+                upperCol -= colDirection;
+            }
+
+            diagListUpper.Reverse();
 
             bool[] diagArr = new bool[diagListUpper.Count + diagListLower.Count + 1];
-
-            for(var i = 0; i < diagListLower.Count; i++)
-            {
-                diagArr[i] = diagListLower[i];
-            }
-
-            diagArr[diagListLower.Count] = true;
-
-            int index = diagListUpper.Count + 1;
+            
             for (var i = 0; i < diagListUpper.Count; i++)
             {
-                diagArr[index] = diagListUpper[i];
+                diagArr[i] = diagListUpper[i];
+
             }
 
+            diagArr[diagListUpper.Count] = true; // putting the current tile played as true
+            var offset = diagListUpper.Count + 1;
+            for (var i = 0; i < diagListLower.Count; i++)
+            {
+                diagArr[i + offset] = diagListLower[i];
+            }
             return diagArr;
         }
 
         private bool getResult(bool[] arr)
         {
             int count = 0;
-
             for (var i = 0; i < arr.Length; i++)
             {
-                if (arr[i] == true)
-                {
-                    count++;
-                }
-                else if (arr[i] == false)
-                {
-                    count = 0;
-                }
-
+                count = arr[i] ? count + 1 : 0;
                 if (count == maxInARow)
                 {
                     return true;
                 }
             }
             return false;
-        }
-
-  
+        } 
 
         private int getTileRow(PictureBox tile)
         {
-            PictureBox[] arrayOfTiles = allTiles.ToArray();
-            int tileIndex = Array.IndexOf(arrayOfTiles, tile);
-            return tileIndex / gridSize;            
+            return Array.IndexOf(allTiles, tile) / gridSize;            
         }
 
         private int getTileCol(PictureBox tile)
         {
-            PictureBox[] arrayOfTiles = allTiles.ToArray();
-            int tileIndex = Array.IndexOf(arrayOfTiles, tile);
-            return tileIndex % gridSize;
+            return Array.IndexOf(allTiles, tile) % gridSize;
         }
 
         private void getCurrentState()
         {
-           // int row = 0;
-           // int col = 0;
-
-            for(var tile = 0; tile < allTiles.Count; tile++)
+            for(var tile = 0; tile < allTiles.Length; tile++)
             {
                 currentStateOfBoard[tile / gridSize, tile % gridSize] = (String)allTiles[tile].Tag;
-
-               /* if(col >= gridSize) // gridSize = 10
-                {
-                    row++;
-                    col = 0;
-                    currentStateOfBoard[row, col] = (String)allTiles[tile].Tag;
-                    col++;
-                }
-                else
-                {
-                    currentStateOfBoard[row, col] = (String)allTiles[tile].Tag;
-                    col++;
-                }*/
             }
         }
 
@@ -264,7 +223,7 @@ namespace XInARow
         {
             bool allTilesTakenWithNoWin = true;
 
-            for (var tile = 0; tile < allTiles.Count; tile++)
+            for (var tile = 0; tile < allTiles.Length; tile++)
             {
                 if (allTiles[tile].Tag == null)
                 {
@@ -279,7 +238,7 @@ namespace XInARow
         private void resetButton_Click(object sender, EventArgs e)
         {
 
-            for (var tile = 0; tile < allTiles.Count; tile++)
+            for (var tile = 0; tile < allTiles.Length; tile++)
             {
                 allTiles[tile].Tag = null;
                 allTiles[tile].Image = null;
